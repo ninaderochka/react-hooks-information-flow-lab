1f2d4d1b4de944f27695693f15b049b75bb6ac8c
Nina Hawari <nina.hawari@gmail.com>
2023-01-25 23:02:06 +0200

    Completed Assignment

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { useState} from 'react'
+
+function Header({onDarkModeClick}) {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+  return (
+    <div>     
+         <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+      {isDarkMode ? "Dark" : "Light"} Mode
+    </button>
+  </header>
+  </div>
+  )
+}
+
+export default Header
